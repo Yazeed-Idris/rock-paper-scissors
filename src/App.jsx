d465a688c0423f81ca3c1950b66b15caea551dbe
yazed <s201730270@kfupm.edu.sa>
2023-03-16 03:38:05 +0300

    - Created the basic style for the moves

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import {Score} from "./components/Score";
 import {Header} from "./components/Header";
+import {Moves} from "./components/Moves";
 
 function App() {
 
   return (
-    <div className="App h-full">
+    <div className="App h-full flex flex-col justify-around">
        <Header score={12} />
+        <Moves />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,12 @@ import {Score} from "./Score";
 export const Header = ({score}) => {
     return <div className='container flex justify-between mx-auto items-center py-4 px-6 border-2 rounded-xl border-Header-Outline bg-transparent text-white'>
         <div>
-            <h1 className='font-Barlow-Semi-Condensed font-[700] leading-6 text-3xl'>
+            <h1 className='font-Barlow-Semi-Condensed font-[700] leading-7 text-4xl'>
                 ROCK
                 <br/>PAPER
                 <br/>SCISSORS
             </h1>
         </div>
-        <Score score={12} />
-
+        <Score score={score} />
     </div>
 }
