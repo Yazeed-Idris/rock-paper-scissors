42a2ebdac5ee9022f22a820844d65327b9f24ac9
yazed <s201730270@kfupm.edu.sa>
2023-03-18 23:47:06 +0300

    - Changed the layout of the moves to use grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Header} from "./components/Header";
 import { useState } from "react";
-import {PaperMoveBadge} from "./components/PaperMoveBadge.jsx";
+import {MoveBadge} from "./components/MoveBadge.jsx";
 
 function App() {
 
@@ -9,9 +9,26 @@ function App() {
   return (
     <div className="App h-full">
        <Header score={score} />
-        <div className='flex items-center'>
-            <PaperMoveBadge move={'paper'}/>
-            <PaperMoveBadge move={'scissors'}/></div>
+        <div className='container w-fit relative mx-auto'>
+            <div className='grid grid-cols-3 grid-rows-2 place-items-center w-full absolute '>
+                <div className='col-start-1 row-start-1'>
+
+                <MoveBadge move={'paper'}/>
+                </div>
+
+                <div className='col-start-3 row-start-1'>
+
+                <MoveBadge move={'scissors'}/>
+                </div>
+
+                <div className='col-start-2 row-start-2'>
+
+                <MoveBadge move={'rock'} />
+                </div>
+            </div>
+            <img src="/src/assets/images/bg-triangle.svg" className='' alt="triangle background"/>
+        </div>
+
 
     </div>
   )
--- src/components/MoveBadge.jsx
+++ src/components/MoveBadge.jsx
@@ -1,16 +1,16 @@
-export const PaperMoveBadge = ({move}) => {
+export const MoveBadge = ({move}) => {
 
     if (move === 'paper') {
         return (
 
-            <div className='bg-Paper-Gradient-from w-fit rounded-full mx-auto'>
+            <div className='bg-Paper-Gradient-from w-fit rounded-full'>
                 <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                     <div
                         className='bg-gradient-to-b from-Paper-Gradient-from to-Paper-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                         <div className='bg-white w-fit rounded-full'>
                             <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                                <div className='bg-white w-fit rounded-full px-12 py-10 opacity-100'>
-                                    <img className='object-fit w-16 h-18' src='/src/assets/images/icon-paper.svg'
+                                <div className='bg-white w-fit rounded-full grid place-items-center w-32 h-32 opacity-100'>
+                                    <img className='object-contain' src='/src/assets/images/icon-paper.svg'
                                          alt='paper badge'/>
                                 </div>
                             </div>
@@ -20,14 +20,14 @@ export const PaperMoveBadge = ({move}) => {
             </div>
         )
     } else if (move === 'rock') {
-        return <div className='bg-Rock-Gradient-from w-fit rounded-full mx-auto'>
+        return <div className='bg-Rock-Gradient-from w-fit rounded-full'>
             <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                 <div
                     className='bg-gradient-to-b from-Rock-Gradient-from to-Rock-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white w-fit rounded-full p-12 opacity-100'>
-                                <img className='object-fit w-16 h-18' src='/src/assets/images/icon-rock.svg'
+                            <div className='bg-white w-fit rounded-full grid place-items-center h-32 w-32 opacity-100'>
+                                <img className='object-contain' src='/src/assets/images/icon-rock.svg'
                                      alt='rock badge'/>
                             </div>
                         </div>
@@ -37,14 +37,14 @@ export const PaperMoveBadge = ({move}) => {
         </div>
 
     } else {
-        return <div className='bg-Scissors-Gradient-from w-fit rounded-full mx-auto'>
+        return <div className='bg-Scissors-Gradient-from w-fit rounded-full'>
             <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                 <div
                     className='bg-gradient-to-b from-Scissors-Gradient-from to-Scissors-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white w-fit rounded-full px-12 py-10 opacity-100'>
-                                <img className='object-fit w-16 h-18' src='/src/assets/images/icon-scissors.svg'
+                            <div className='bg-white w-fit rounded-full grid place-items-center w-32 h-32 opacity-100'>
+                                <img className='object-contain' src='/src/assets/images/icon-scissors.svg'
                                      alt='scissors badge'/>
                             </div>
                         </div>
