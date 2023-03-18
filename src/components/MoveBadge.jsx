cdc8c084f2edc4895c248835710d9b582e7cb57a
yazed <s201730270@kfupm.edu.sa>
2023-03-18 23:50:23 +0300

    - Fixed width bug for badges

--- src/components/MoveBadge.jsx
+++ src/components/MoveBadge.jsx
@@ -9,7 +9,7 @@ export const MoveBadge = ({move}) => {
                         className='bg-gradient-to-b from-Paper-Gradient-from to-Paper-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                         <div className='bg-white w-fit rounded-full'>
                             <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                                <div className='bg-white w-fit rounded-full grid place-items-center w-32 h-32 opacity-100'>
+                                <div className='bg-white rounded-full grid place-items-center w-32 h-32 opacity-100'>
                                     <img className='object-contain' src='/src/assets/images/icon-paper.svg'
                                          alt='paper badge'/>
                                 </div>
@@ -26,7 +26,7 @@ export const MoveBadge = ({move}) => {
                     className='bg-gradient-to-b from-Rock-Gradient-from to-Rock-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white w-fit rounded-full grid place-items-center h-32 w-32 opacity-100'>
+                            <div className='bg-white rounded-full grid place-items-center h-32 w-32 opacity-100'>
                                 <img className='object-contain' src='/src/assets/images/icon-rock.svg'
                                      alt='rock badge'/>
                             </div>
@@ -43,7 +43,7 @@ export const MoveBadge = ({move}) => {
                     className='bg-gradient-to-b from-Scissors-Gradient-from to-Scissors-Gradient-to col-span-1 row-span-1 rounded-full p-6 px-7 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white w-fit rounded-full grid place-items-center w-32 h-32 opacity-100'>
+                            <div className='bg-white rounded-full grid place-items-center w-32 h-32 opacity-100'>
                                 <img className='object-contain' src='/src/assets/images/icon-scissors.svg'
                                      alt='scissors badge'/>
                             </div>
