63713504f00c24f62dc850a4c895d04fad9a3d6f
yazed <s201730270@kfupm.edu.sa>
2023-03-19 00:30:41 +0300

    - Removed mx-auto and mt-12 from the Header component to make it more decoupled from the main app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import {Score} from "./Score";
 
 export const Header = ({score}) => {
-    return <div className='container mt-12 flex justify-between mx-auto items-center py-4 px-6 border-2 rounded-xl border-Header-Outline bg-transparent text-white'>
+    return <div className='container flex justify-between items-center py-4 px-6 border-2 rounded-xl border-Header-Outline bg-transparent text-white'>
         <div>
             <h1 className='font-Barlow-Semi-Condensed font-[700] leading-7 text-4xl'>
                 ROCK
