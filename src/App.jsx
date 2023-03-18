8a6fda5f565d06a5de747daadd1f1a5d12e2f2d2
yazed <s201730270@kfupm.edu.sa>
2023-03-19 02:03:57 +0300

    - Positioning Components and editing margins and paddings. Also created the rules button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import {Header} from "./components/Header";
-import { useState } from "react";
+import {useState} from "react";
 import {MovesContainer} from "./components/MovesContainer.jsx";
-
+import {RulesButton} from "./components/RulesButton.jsx";
 
 
 function App() {
 
     const [score, setScore] = useState(0);
 
-  return (
-    <div className="App h-full">
-        <Header score={score}/>
-        <MovesContainer/>
-    </div>
-  )
-}
+    return (
+        <div className='App flex flex-col justify-between h-full items-center'>
+            <div className="flex flex-col items-center container">
+                <div className='mt-12 mb-20 grid place-items-center'>
+                    <Header score={score}/>
+                </div>
+                <MovesContainer/>
+            </div>
+            <div className='mb-12 w-full lg:mr-24'>
+                <div className='w-full flex justify-center lg:justify-end '>
+                    <RulesButton/>
+                </div>
+            </div>
+        </div>
 
+    )
+}
 
 
 export default App
---
+++ src/components/RulesButton.jsx
@@ -0,0 +1,5 @@
+export const RulesButton = () => {
+    return <button className='bg-transparent border-2 border border-white rounded-2xl text-white text-3xl tracking-widest font-Barlow-Semi-Condensed font-[600] px-12 py-3'>
+        RULES
+    </button>
+}
