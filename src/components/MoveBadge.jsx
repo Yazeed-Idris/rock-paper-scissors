a6f2c7d753e7611aaab91eccc9acaf68dd09fdec
yazed <s201730270@kfupm.edu.sa>
2023-03-19 05:15:32 +0300

    - Added Logic to make other badges disappear when one is clicked

--- src/components/MoveBadge.jsx
+++ src/components/MoveBadge.jsx
@@ -1,16 +1,23 @@
-export const MoveBadge = ({move}) => {
+export const MoveBadge = ({move, setMoveNumber}) => {
+
+    function handleClick(moveNumber) {
+        setMoveNumber(moveNumber)
+    }
 
     if (move === 'paper') {
         return (
-
-            <div className='bg-Paper-Gradient-from w-fit rounded-full'>
+            <div onClick={() => {
+                handleClick(1)
+            }} className='bg-Paper-Gradient-from w-fit rounded-full'>
                 <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                     <div
                         className='bg-gradient-to-b from-Paper-Gradient-from to-Paper-Gradient-to col-span-1 row-span-1 rounded-full p-4 px-5 sm:p-5 sm:px-6 w-fit'>
                         <div className='bg-white w-fit rounded-full'>
                             <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                                <div className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
-                                    <img className='object-contain sm:w-24 sm:h-20' src='/src/assets/images/icon-paper.svg'
+                                <div
+                                    className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
+                                    <img className='object-contain sm:w-24 sm:h-20'
+                                         src='/src/assets/images/icon-paper.svg'
                                          alt='paper badge'/>
                                 </div>
                             </div>
@@ -20,13 +27,16 @@ export const MoveBadge = ({move}) => {
             </div>
         )
     } else if (move === 'rock') {
-        return <div className='bg-Rock-Gradient-from w-fit rounded-full'>
+        return <div onClick={() => {
+            handleClick(2)
+        }} className='bg-Rock-Gradient-from w-fit rounded-full'>
             <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                 <div
                     className='bg-gradient-to-b from-Rock-Gradient-from to-Rock-Gradient-to col-span-1 row-span-1 rounded-full p-4 px-5 sm:p-5 sm:px-6 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
+                            <div
+                                className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
                                 <img className='object-contain sm:w-20 sm:h-16' src='/src/assets/images/icon-rock.svg'
                                      alt='rock badge'/>
                             </div>
@@ -37,14 +47,18 @@ export const MoveBadge = ({move}) => {
         </div>
 
     } else {
-        return <div className='bg-Scissors-Gradient-from w-fit rounded-full'>
+        return <div onClick={() => {
+            handleClick(3)
+        }} className='bg-Scissors-Gradient-from w-fit rounded-full'>
             <div className='bg-black bg-opacity-30 w-fit rounded-full pb-2'>
                 <div
                     className='bg-gradient-to-b from-Scissors-Gradient-from to-Scissors-Gradient-to col-span-1 row-span-1 rounded-full p-4 px-5 sm:p-5 sm:px-6 w-fit'>
                     <div className='bg-white w-fit rounded-full'>
                         <div className='bg-black rounded-full pt-2 bg-opacity-25'>
-                            <div className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
-                                <img className='object-contain sm:w-24 sm:h-20' src='/src/assets/images/icon-scissors.svg'
+                            <div
+                                className='bg-white rounded-full grid place-items-center w-26 h-24 sm:w-36 sm:h-34 opacity-100'>
+                                <img className='object-contain sm:w-24 sm:h-20'
+                                     src='/src/assets/images/icon-scissors.svg'
                                      alt='scissors badge'/>
                             </div>
                         </div>
--- src/components/MovesContainer.jsx
+++ src/components/MovesContainer.jsx
@@ -1,23 +1,29 @@
 import {MoveBadge} from "./MoveBadge.jsx";
+import {useState} from "react";
 
 export const MovesContainer = () => {
+
+    const [moveNumber, setMoveNumber] = useState(4);
+
     return <div className="container w-fit h-fit p-20 relative ">
         <div className="grid grid-cols-3 grid-rows-2 place-items-center w-full h-full absolute top-0 left-0">
+
             <div className='flex col-start-1 col-end-4 justify-around sm:justify-between w-full'>
-                <div>
-
-                    <MoveBadge move={"paper"}/>
-                </div>
-
-                <div>
-
-                    <MoveBadge move={"scissors"}/>
-                </div>
+                {moveNumber === 4 || moveNumber === 1 ?
+                    <div>
+                        <MoveBadge setMoveNumber={setMoveNumber} move={"paper"}/>
+                    </div> : <></>}
+                {moveNumber === 4 || moveNumber === 3 ?
+                    <div>
+                        <MoveBadge setMoveNumber={setMoveNumber} move={"scissors"}/>
+                    </div>
+                    : <></>}
             </div>
 
             <div className="col-start-2 row-start-2 mb-4">
-
-                <MoveBadge move={"rock"}/>
+                {moveNumber === 4 || moveNumber === 2 ?
+                    <MoveBadge setMoveNumber={setMoveNumber} move={"rock"}/>
+                    : <></>}
             </div>
         </div>
         <img src="/src/assets/images/bg-triangle.svg" className="" alt="triangle background"/>
