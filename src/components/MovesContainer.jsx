294aaaeb3e88066492d434117fad0f07a30b792b
yazed <s201730270@kfupm.edu.sa>
2023-03-19 05:02:40 +0300

    - Refactoring classes and formatting

--- src/components/MovesContainer.jsx
+++ src/components/MovesContainer.jsx
@@ -4,12 +4,12 @@ export const MovesContainer = () => {
     return <div className="container w-fit h-fit p-20 relative ">
         <div className="grid grid-cols-3 grid-rows-2 place-items-center w-full h-full absolute top-0 left-0">
             <div className='flex col-start-1 col-end-4 justify-around sm:justify-between w-full'>
-                <div className="d ">
+                <div>
 
                     <MoveBadge move={"paper"}/>
                 </div>
 
-                <div className="">
+                <div>
 
                     <MoveBadge move={"scissors"}/>
                 </div>
