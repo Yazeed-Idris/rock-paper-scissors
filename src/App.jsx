7223dd1ff1be7fc2e16564ca332dedc74cc78d53
yazed <s201730270@kfupm.edu.sa>
2023-03-20 08:29:43 +0300

    - Created exit animation and enter animation for the moves and match containers

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,23 @@ import {RulesButton} from "./components/RulesButton.jsx";
 function App() {
 
     const [score, setScore] = useState(0);
+    const [moveClicked, setMoveClicked] = useState(false);
+
+    function moveClickedHandler() {
+        setMoveClicked(true);
+    }
 
     return (
-        <div className='App flex flex-col justify-between h-full items-center'>
+        <div className='App flex flex-col justify-between h-full items-center overflow-x-hidden'>
             <div className="flex flex-col items-center container">
                 <div className='mt-12 mb-20 grid place-items-center'>
                     <Header score={score}/>
                 </div>
-                <MovesContainer/>
+                {!moveClicked ? <MovesContainer moveClickedHandler={moveClickedHandler}/> : ''}
+                <div
+                    className={`${!moveClicked ? 'translate-x-[100vw]' : ''} transition-all duration-100`}>
+                    <h1>Hello world</h1>
+                </div>
             </div>
             <div className='mb-12 w-full lg:mr-24'>
                 <div className='w-full flex justify-center lg:justify-end '>
--- src/components/MovesContainer.jsx
+++ src/components/MovesContainer.jsx
@@ -1,38 +1,34 @@
 import {MoveBadge} from "./MoveBadge.jsx";
 import {useState} from "react";
 
-export const MovesContainer = () => {
+export const MovesContainer = ({moveClickedHandler}) => {
 
-    const [moveNumber, setMoveNumber] = useState(4);
+    const [clicked, setClicked] = useState(false);
 
-    return <div className=" w-full h-fit p-20 relative ">
-        <div
-            className={`${moveNumber !== 4 ? 'flex justify-around' : 'grid'} grid-cols-3 grid-rows-2 place-items-center w-full h-full absolute top-0 left-0`}>
+    function handleClick() {
+        setClicked(true)
+        setTimeout(() => {
+            moveClickedHandler()
+        }, 300)
+    }
 
-            <div
-                className={`flex col-start-1 col-end-4 justify-around sm:justify-between w-full ${moveNumber !== 4 ? '' : ''}`}>
-                <div className={`${moveNumber !== 1 ? 'invisible' : ''}`}>
-                    <MoveBadge setMoveNumber={setMoveNumber} move={"paper"}/>
+    return <div
+        className={`container w-fit h-fit p-20 ${clicked ? '-translate-x-[100vw] transition-all duration-300' : ''} relative`}>
+        <div className="grid grid-cols-3 grid-rows-2 place-items-center w-full h-full absolute top-0 left-0">
+
+            <div className='flex col-start-1 col-end-4 justify-around sm:justify-between w-full'>
+                <div onClick={handleClick}>
+                    <MoveBadge move={"paper"}/>
                 </div>
-                {moveNumber === 4 || moveNumber === 3 ?
-                    <div
-                        className={`${moveNumber === 3 ? 'scale-125 transition-all duration-700 left-0 top-0' : 'left-1/2'} absolute`}>
-                        <MoveBadge setMoveNumber={setMoveNumber} move={"scissors"}/>
-                    </div>
-                    : <div></div>}
+                <div onClick={handleClick}>
+                    <MoveBadge move={"scissors"}/>
+                </div>
             </div>
 
-            <div className={`${moveNumber !== 4 ? 'scale-100' : 'scale-0'} transition-all duration-700`}>
-                <h1 className='font-Barlow-Semi-Condensed font-[700] text-white text-4xl'>Hello World!</h1>
-            </div>
-            <div className="col-start-2 row-start-2 mb-4">
-                {moveNumber === 4 || moveNumber === 2 ?
-                    <MoveBadge setMoveNumber={setMoveNumber} move={"rock"}/>
-                    : <div></div>}
+            <div onClick={handleClick} className="col-start-2 row-start-2 mb-4">
+                <MoveBadge move={"rock"}/>
             </div>
         </div>
-        <img src="/src/assets/images/bg-triangle.svg" className={`mx-auto ${moveNumber !== 4 ? 'invisible' : ''}`}
-             alt="triangle background"/>
-
+        <img src="/src/assets/images/bg-triangle.svg" className="" alt="triangle background"/>
     </div>;
 }
