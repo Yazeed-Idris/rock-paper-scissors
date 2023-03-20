2f0cfbde62cf8367da2f7d0358c7bb5188d6a24e
yazed <s201730270@kfupm.edu.sa>
2023-03-20 08:09:11 +0300

    - Reverted to previous commit (animations are very complex :( )

--- src/components/MovesContainer.jsx
+++ src/components/MovesContainer.jsx
@@ -9,21 +9,16 @@ export const MovesContainer = () => {
         <div className="grid grid-cols-3 grid-rows-2 place-items-center w-full h-full absolute top-0 left-0">
 
             <div className='flex col-start-1 col-end-4 justify-around sm:justify-between w-full'>
-                {moveNumber === 4 || moveNumber === 1 ?
-                    <div>
-                        <MoveBadge setMoveNumber={setMoveNumber} move={"paper"}/>
-                    </div> : <></>}
-                {moveNumber === 4 || moveNumber === 3 ?
-                    <div>
-                        <MoveBadge setMoveNumber={setMoveNumber} move={"scissors"}/>
-                    </div>
-                    : <></>}
+                <div>
+                    <MoveBadge setMoveNumber={setMoveNumber} move={"paper"}/>
+                </div>
+                <div>
+                    <MoveBadge setMoveNumber={setMoveNumber} move={"scissors"}/>
+                </div>
             </div>
 
             <div className="col-start-2 row-start-2 mb-4">
-                {moveNumber === 4 || moveNumber === 2 ?
-                    <MoveBadge setMoveNumber={setMoveNumber} move={"rock"}/>
-                    : <></>}
+                <MoveBadge setMoveNumber={setMoveNumber} move={"rock"}/>
             </div>
         </div>
         <img src="/src/assets/images/bg-triangle.svg" className="" alt="triangle background"/>
