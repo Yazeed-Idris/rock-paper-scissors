fd9383347bc41af58098c3bec3ea6de422a438cd
yazed <s201730270@kfupm.edu.sa>
2023-03-20 20:08:32 +0300

    - Moved images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
     }
 
     return (
-        <div className='App flex flex-col justify-between h-full items-center overflowhidden '>
+        <div className='App flex flex-col justify-between h-full items-center overflow-hidden '>
             <div className="flex flex-col items-center container">
                 <div className='mt-12 sm:w-full mb-20 grid place-items-center'>
                     <Header score={score}/>
